46a5771440be70ac6ae90b6b4d6dc3fc6eedaa29
loucimf <145812460+loucimf@users.noreply.github.com>
2026-03-21 14:55:39 -0300

    Added openmeteo API

---
+++ src/components/tabs/components/DashComps.tsx
@@ -0,0 +1,285 @@
+import ClickableWrapper from "@src/components/ClickableWrapper"
+import { CardContainer, HorizontalContainer, VerticalContainer } from "@src/components/containers"
+import { SystemIcon } from "@src/design-system/SystemIcon"
+import { SmallText, Text } from "@src/design-system/Texts"
+import { H1Title, H2Title } from "@src/design-system/Titles"
+import { designSystemStyles } from "@src/styles/designSystemStyles"
+import { useState, type ComponentProps } from "react"
+
+const AirQualityComp: React.FC = () => {
+    return (
+        <CardContainer
+            padding={designSystemStyles.paddingLg}
+            height="50%"
+        >
+            <HorizontalContainer>
+                <IconText text="Air Quality Index" bold={true}/>
+            </HorizontalContainer>
+            <HorizontalContainer>
+                <H1Title text="41"/>
+                <CardContainer
+                    width="50px"
+                    height="auto"
+                    padding={"5px"}
+                    backgroundColor={designSystemStyles.colorTintedAction}
+                >
+                    <HorizontalContainer
+                        width="100%"
+                        justifyContent="center"
+                        alignment="center"
+                    >
+                        <SmallText color={designSystemStyles.colorMainAction} markdown={true} text="**Good**"/>
+                    </HorizontalContainer>
+                </CardContainer>
+            </HorizontalContainer>
+            <div style={{ height: "1vh", width: "100%", backgroundColor: designSystemStyles.colorBackgroundGray3, borderRadius: designSystemStyles.radiusItem, marginBottom: "10px"}}>
+                <div style={{ height: "100%", width: "20%", position: "relative", left: "0", top: "0", backgroundColor: designSystemStyles.colorGreen , borderRadius: designSystemStyles.radiusItem}}/>
+            </div>
+            <SmallText markdown={true} text="**Air quality is considered satisfactory, and air pollution poses little to no risk**"/>
+        </CardContainer>
+    )
+}
+
+const SunAndMoonComp: React.FC = () => {
+    return (
+        <CardContainer
+            height="50%"
+            padding={designSystemStyles.paddingLg}
+        >
+            <VerticalContainer
+                width="100%"
+                height="100%"
+                justifyContent="space-between"
+            >
+                <HorizontalContainer>
+                    <IconText text="Sun and Moon" bold={true} />
+                </HorizontalContainer>
+                <HorizontalContainer
+                    width="100%"
+                    alignment="center"
+                    justifyContent="space-between"
+                >
+                    <TimeComp
+                        hour="5:45 AM"
+                        text="Sunrise"
+                    />
+                    <SystemIcon type="edit" color="black"/>
+                    <TimeComp
+                        hour="9:25 PM"
+                        text="Sunset"
+                    />
+                </HorizontalContainer>
+            </VerticalContainer>
+        </CardContainer>
+    )
+}
+
+const TimeComp: React.FC<{
+    hour: string
+    text: string
+}> = ({
+    text,
+    hour
+}) => {
+    return (
+        <VerticalContainer
+            width="fit-content"
+            alignment="center"
+            gap="0"
+            padding="0"
+        >
+            <SystemIcon type="edit" color="black"/>
+            <Text markdown={true} text={`**${hour}**`}/>
+            <SmallText text={text}/>
+        </VerticalContainer>
+    )
+}
+
+interface OverviewCompProps {
+    temp: number,
+    city: string,
+    date: string,
+    wind: string,
+    humidity: string,
+    visibility: string,
+    pressure: string,
+    weatherState: string,
+}
+
+const OverviewComp: React.FC<OverviewCompProps> = ({
+    city,
+    pressure,
+    wind,
+    humidity,
+    date,
+    visibility,
+    weatherState,
+    temp
+}) => {
+    const [useCelsius, setUseCelsius] = useState<boolean>(true)
+
+    return (
+        <CardContainer
+            width="65%"
+            height="100%"
+            padding={designSystemStyles.paddingLg}
+        >
+            <VerticalContainer
+                height={"100%"}
+                width="100%"
+                justifyContent="space-between"
+            >
+                <HorizontalContainer
+                    justifyContent="space-between"
+                    alignment="center"
+                    height={"20%"}
+                >
+                    <VerticalContainer
+                        gap="0"
+                        margin={"0"}
+                    >
+                        <HorizontalContainer
+                            gap={designSystemStyles.gapSm}
+                            margin={0}
+                        >
+                            <SystemIcon type="calendar" color="black" />
+                            <Text markdown={true} text={`**${city}**`} />
+                        </HorizontalContainer>
+                        <SmallText text={`Today, ${date}`} />
+                    </VerticalContainer>
+
+
+                    <ClickableWrapper onClick={() => { 
+                        setUseCelsius(!useCelsius)
+                    }}>
+                        <CardContainer>
+                            <HorizontalContainer
+                                height={"10px"}
+                            >
+                                <Text markdown={true} text={useCelsius ? "**°C**" : "°C"} />
+                                <div style={{ height: "100%", width: "1px", backgroundColor: "black" }} />
+                                <Text markdown={true} text={!useCelsius ? "**°F**" : "°F"} />
+                            </HorizontalContainer>
+                        </CardContainer>
+                    </ClickableWrapper>
+                </HorizontalContainer>
+                <HorizontalContainer
+                    width="100%"
+                    height={"80%"}
+                    justifyContent="space-between"
+                    alignment="end"
+                >
+                    <HorizontalContainer
+                        width="60%"
+                        height={"50%"}
+                    >
+                        <div style={{ height: "100px ", width: "100px", borderRadius: "50%", backgroundColor: "grey" }} />
+                        <VerticalContainer
+                            gap="0"
+                            margin={0}
+                            width="25%"
+                        >
+                            <h1 style={{ margin: "0" }}>{ useCelsius ? `${temp}°` : `${convertToFahrenheit(temp)}°`}</h1>
+                            <Text text={weatherState} />
+                        </VerticalContainer>
+                    </HorizontalContainer>
+                    <HorizontalContainer
+                        width="40%"
+                        height={"50%"}
+                    >
+                        <CardContainer
+                            backgroundColor={designSystemStyles.colorBackgroundGray2}
+                        >
+                            <VerticalContainer
+                                gap={designSystemStyles.gapSm}
+                            >
+                                <HorizontalContainer>
+                                    <DataPoint text="Wind" value={wind}/>
+                                    <DataPoint text="Humidity" value={humidity}/>      
+                                </HorizontalContainer>
+                                <HorizontalContainer>
+                                    <DataPoint text="Visibility" value={visibility} icon="eye"/>
+                                    <DataPoint text="Pressure" value={pressure}/>      
+                                </HorizontalContainer>
+                            </VerticalContainer>
+
+                        </CardContainer>
+                    </HorizontalContainer>
+                </HorizontalContainer>
+            </VerticalContainer>
+        </CardContainer>
+    )
+}
+
+const Header: React.FC<{
+    height: string
+    width: string
+}> = ({
+    height,
+    width
+}) => {
+    return (
+        <VerticalContainer
+            height={height}
+            width={width}
+            gap="0"
+            margin={"0"}
+        >
+            <H2Title text="Good morning, Ivo" />
+            <Text text="Here is your weather overview for today" />
+        </VerticalContainer>
+    )
+}
+
+
+const IconText: React.FC<{
+    text: string,
+    bold?: boolean,
+    icon?: ComponentProps<typeof SystemIcon>["type"]
+}> = ({
+    text,
+    bold = false,
+    icon = "edit"
+}) => {
+    return (
+        <HorizontalContainer>
+            <SystemIcon type={icon} color="black"/>
+            <Text markdown={bold ? true : false} text={bold ? `**${text}**` : text}/>
+        </HorizontalContainer>
+    )
+}
+
+
+const DataPoint: React.FC<{
+    text: string,
+    value: string,
+    icon?: ComponentProps<typeof SystemIcon>["type"]
+}> = ({ 
+    text = "data",
+    value = "n/a",
+    icon = "edit"
+}) => {
+    return (
+        <VerticalContainer
+            gap="0"
+        >
+            <HorizontalContainer
+                gap={designSystemStyles.gapSm}
+                margin={0}
+            >
+                <SystemIcon type={icon} color="black" />
+                <SmallText text={text} />
+            </HorizontalContainer>
+            <Text markdown={true} text={`**${value}**`} />
+        </VerticalContainer>
+    )
+}
+
+
+function convertToFahrenheit(temp:number) {
+    const converted = (temp / (9/5)) + 32
+    return Math.round(converted)
+} 
+
+
+export { Header, OverviewComp, AirQualityComp, SunAndMoonComp }
---
+++ src/core/weatherData.ts
@@ -0,0 +1,103 @@
+import { fetchWeatherApi } from "openmeteo";
+
+const params = {
+	latitude: 52.52,
+	longitude: 13.41,
+	daily: ["sunrise", "sunset", "uv_index_max", "temperature_2m_max", "temperature_2m_min"],
+	hourly: ["temperature_2m", "apparent_temperature", "precipitation_probability", "wind_speed_10m", "visibility", "surface_pressure", "cloud_cover", "uv_index", "sunshine_duration"],
+	current: ["temperature_2m", "cloud_cover", "wind_speed_10m", "surface_pressure", "precipitation", "showers", "rain", "apparent_temperature", "relative_humidity_2m"],
+	timezone: "America/New_York",
+};
+const url = "https://api.open-meteo.com/v1/forecast";
+const responses = await fetchWeatherApi(url, params);
+
+// Process first location. Add a for-loop for multiple locations or weather models
+const response = responses[0];
+
+// Attributes for timezone and location
+const latitude = response.latitude();
+const longitude = response.longitude();
+const elevation = response.elevation();
+const timezone = response.timezone();
+const timezoneAbbreviation = response.timezoneAbbreviation();
+const utcOffsetSeconds = response.utcOffsetSeconds();
+
+console.log(
+	`\nCoordinates: ${latitude}°N ${longitude}°E`,
+	`\nElevation: ${elevation}m asl`,
+	`\nTimezone: ${timezone} ${timezoneAbbreviation}`,
+	`\nTimezone difference to GMT+0: ${utcOffsetSeconds}s`,
+);
+
+const current = response.current()!;
+const hourly = response.hourly()!;
+const daily = response.daily()!;
+
+// Define Int64 variables so they can be processed accordingly
+const sunrise = daily.variables(0)!;
+const sunset = daily.variables(1)!;
+
+// Note: The order of weather variables in the URL query and the indices below need to match!
+const weatherData = {
+	current: {
+		time: new Date((Number(current.time()) + utcOffsetSeconds) * 1000),
+		temperature_2m: current.variables(0)!.value(),
+		cloud_cover: current.variables(1)!.value(),
+		wind_speed_10m: current.variables(2)!.value(),
+		surface_pressure: current.variables(3)!.value(),
+		precipitation: current.variables(4)!.value(),
+		showers: current.variables(5)!.value(),
+		rain: current.variables(6)!.value(),
+		apparent_temperature: current.variables(7)!.value(),
+		relative_humidity_2m: current.variables(8)!.value(),
+	},
+	hourly: {
+		time: Array.from(
+			{ length: (Number(hourly.timeEnd()) - Number(hourly.time())) / hourly.interval() }, 
+			(_ , i) => new Date((Number(hourly.time()) + i * hourly.interval() + utcOffsetSeconds) * 1000)
+		),
+		temperature_2m: hourly.variables(0)!.valuesArray(),
+		apparent_temperature: hourly.variables(1)!.valuesArray(),
+		precipitation_probability: hourly.variables(2)!.valuesArray(),
+		wind_speed_10m: hourly.variables(3)!.valuesArray(),
+		visibility: hourly.variables(4)!.valuesArray(),
+		surface_pressure: hourly.variables(5)!.valuesArray(),
+		cloud_cover: hourly.variables(6)!.valuesArray(),
+		uv_index: hourly.variables(7)!.valuesArray(),
+		sunshine_duration: hourly.variables(8)!.valuesArray(),
+	},
+	daily: {
+		time: Array.from(
+			{ length: (Number(daily.timeEnd()) - Number(daily.time())) / daily.interval() }, 
+			(_ , i) => new Date((Number(daily.time()) + i * daily.interval() + utcOffsetSeconds) * 1000)
+		),
+		// Map Int64 values to according structure
+		sunrise: [...Array(sunrise.valuesInt64Length())].map(
+			(_ , i) => new Date((Number(sunrise.valuesInt64(i)) + utcOffsetSeconds) * 1000)
+		),
+		// Map Int64 values to according structure
+		sunset: [...Array(sunset.valuesInt64Length())].map(
+			(_ , i) => new Date((Number(sunset.valuesInt64(i)) + utcOffsetSeconds) * 1000)
+		),
+		uv_index_max: daily.variables(2)!.valuesArray(),
+		temperature_2m_max: daily.variables(3)!.valuesArray(),
+		temperature_2m_min: daily.variables(4)!.valuesArray(),
+	},
+};
+
+// The 'weatherData' object now contains a simple structure, with arrays of datetimes and weather information
+console.log(
+	`\nCurrent time: ${weatherData.current.time}\n`,
+	`\nCurrent temperature_2m: ${weatherData.current.temperature_2m}`,
+	`\nCurrent cloud_cover: ${weatherData.current.cloud_cover}`,
+	`\nCurrent wind_speed_10m: ${weatherData.current.wind_speed_10m}`,
+	`\nCurrent surface_pressure: ${weatherData.current.surface_pressure}`,
+	`\nCurrent precipitation: ${weatherData.current.precipitation}`,
+	`\nCurrent showers: ${weatherData.current.showers}`,
+	`\nCurrent rain: ${weatherData.current.rain}`,
+	`\nCurrent apparent_temperature: ${weatherData.current.apparent_temperature}`,
+	`\nCurrent relative_humidity_2m: ${weatherData.current.relative_humidity_2m}`,
+);
+console.log("\nHourly data:\n", weatherData.hourly)
+console.log("\nDaily data:\n", weatherData.daily)
+
